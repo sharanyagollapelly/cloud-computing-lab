f19047917bf861dcc847b562afea6f5f843f2cf1
sharanyagollapelly <sharanyagollapelli@gmail.com>
2026-08-21 15:44:39 +0530

    Modify home route to return HTML response
    Updated the home route to return HTML content instead of a string.

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-   <html>
-   <body>
-   <h1>Hello!! RGUKT</h1>
-   </body>
-   </html>
-   return "flask application running successfully"
+    return """
+    <html>
+    <body>
+        <h1>Hello!! RGUKT</h1>
+    </body>
+    </html>
+    """
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask
+# app=Flask(__name__)
+# @app.route("/")
+# def home():
+#    <html>
+#    <body>
+#    <h1>Hello!! RGUKT</h1>
+#    </body>
+#    </html>
+#    return "flask application running successfully"
+# if __name__ == "__main__":
+#     app.run(debug=True)
